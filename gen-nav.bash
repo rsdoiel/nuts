6b5d97ed02dfbb8d4b08ed4a45ebb23d00ba8eb7
R. S. Doiel <rsdoiel@gmail.com>
2017-12-09 08:41:29 -0800

    Quick Save

--- gen-nav.bash
+++ gen-nav.bash
@@ -6,18 +6,18 @@ GIT_REPO="https://github.com/rsdoiel/nuts"
 # docs
 # how to
 for D in "" "docs/" "how-to/"; do
-		echo "Writing ${D}nav.md"
-        if [[ "$D" = "" ]]; then
-RELPATH=""
-else
+	echo "Writing ${D}nav.md"
+	if [[ "$D" == "" ]]; then
+		RELPATH=""
+	else
 		RELPATH=$(reldocpath "${D}" .)
-    fi
-		mkpage nav.tmpl relroot="text:${RELPATH}" \
-			readme="text:index.html" \
-			license="text:license.html" \
-			install="text:install.html" \
-			docs="text:docs/" \
-			howto="text:how-to/" \
-			gitrepo="text:${GIT_REPO}" \
-			>"${D}nav.md"
+	fi
+	mkpage nav.tmpl relroot="text:${RELPATH}" \
+		readme="text:index.html" \
+		license="text:license.html" \
+		install="text:install.html" \
+		docs="text:docs/" \
+		howto="text:how-to/" \
+		gitrepo="text:${GIT_REPO}" \
+		>"${D}nav.md"
 done
