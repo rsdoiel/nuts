92b89210bf9fcf7ae7fd203bb84a517b199c2bfa
R. S. Doiel <rsdoiel@gmail.com>
2017-12-13 07:21:39 -0800

    Quick Save

--- cmd/cashew/main.go
+++ cmd/cashew/main.go
@@ -9,11 +9,14 @@ import (
 )
 
 func main() {
+	quitButton := tui.NewButton("Quit")
 	box := tui.NewVBox(
 		tui.NewLabel("nuts-cashew"),
+		quitButton,
 	)
 	ui := tui.New(box)
 	ui.SetKeybinding("Esc", func() { ui.Quit() })
+	quitButton.OnActivated(func(b *tui.Button) { ui.Quit() })
 	if err := ui.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 	}
